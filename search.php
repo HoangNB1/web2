5100b17b736570d8edbe7b81423ff45e97f47de5
HoangNB1 <76941083+HoangNB1@users.noreply.github.com>
2021-01-21 15:50:55 +0700

    Update search.php

--- search.php
+++ search.php
@@ -144,7 +144,7 @@
 			<div class="row">
             
             <?php
-		$hostname = "34.200.32.20";
+		$hostname = "34.230.84.57";
 		$user = "root";
 		$pass = "123@123a";
 		$db = "tunesource";
